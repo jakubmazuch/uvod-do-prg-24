05854503657952c0705fc07b65eae1d58d27f0b6
Jakub Mazuch <jakub.mazuch@gmail.com>
2023-10-12 15:30:47 +0200

    aktualizace ze cvičení 12.10.
    aktualizace ze cvičení 12.10.

---
+++ cv02/kalkulacka.py
@@ -0,0 +1,16 @@
+a = float(input("Zadej číslo a: "))
+b = float(input("Zadej číslo b: "))
+
+soucet = a + b
+rozdil = a - b
+soucin = a * b
+podil = a / b
+modulo = a % b
+expon = a ** b
+
+print(a, "+", b, "= ", soucet)
+print(a, "-", b, "= ", rozdil)
+print(a, "*", b, "= ", soucin)
+print(a, "/", b, "= ", podil)
+print(a, "%", b, "= ", modulo)
+print(a, "^", b, "= ", expon)
